9b55c48925587e32db4fd27e6a471f9492b483c7
Bogdan-Alexandru Maciuca <bogdanmaciuca0621@gmail.com>
2026-03-03 21:45:29 +0200

    org mode proportional fonts

--- init.el
+++ init.el
@@ -8,9 +8,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(afternoon-theme auctex gdscript-mode kanagawa-themes markdown-mode
-		     moody nano-modeline org-bullets super-save
-		     thankful-eyes-theme undo-fu)))
+   '(afternoon-theme auctex gdscript-mode kanagawa-themes markdown-mode moody nano-modeline org-bullets super-save thankful-eyes-theme undo-fu)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
